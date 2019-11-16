4990361f52c149e0dd95159ca05a54ff163134ba
Malakhov Dmitriy <mitia2022@gmail.com>
2019-11-16 23:12:33 +0300

    kek

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,170 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var mongoose = require("mongoose");
+
+var kittySchema = new mongoose.Schema({
+    name: String
+});
+
+const statsSchema = new mongoose.Schema({
+    stat_id: String,
+    last_hit: {
+        time: Number,
+        x: Number,
+        y: Number,
+        strength: Number,
+        id: Number
+    },
+    last_pose: {
+        time_start: Number,
+        time_end: Number,
+        pose_id: Number
+    }
+});
+var Kitten = mongoose.model("Kitten", kittySchema);
+var Stats = mongoose.model("Stats", statsSchema);
+
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/test", {
+    useNewUrlParser: true
+});
+
+const STATS_ID = "arngry";
 
 const sampleRes = {
-  "last_hit": {
-    "time": new Date().getTime(),
-    "x": 100,
-    "y": 120,
-    "strength": 0.4,
-    "id": 21341,
-  },
-  "last_pose": {
-    "time_start": new Date().getTime() - 10000,
-    "time_end": new Date().getTime() - 5000,
-    "pose_id": 1,
-  },
-}
+    last_hit: {
+        time: new Date().getTime(),
+        x: 100,
+        y: 120,
+        strength: 0.4,
+        id: 21341
+    },
+    last_pose: {
+        time_start: new Date().getTime() - 10000,
+        time_end: new Date().getTime() - 5000,
+        pose_id: 1
+    }
+};
 
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+    // we're connected!
+    console.log("db ready");
+});
+
+router.post("/pose", (req, res, next) => {
+    Stats.findOne({ stat_id: STATS_ID }, (err, item) => {
+        if (!!item) {
+            console.log(item);
+            const { last_pose } = req.body;
+
+            Stats.findOneAndUpdate(
+                { stat_id: STATS_ID },
+                { last_pose: { ...last_pose } },
+                (err, r) => {
+                    if (!err) {
+                        console.log(r, last_pose);
+                        res.json({
+                            stats: "updated"
+                        });
+                    } else {
+                        console.log(err);
+                        res.json({
+                            error: "error"
+                        });
+                    }
+                }
+            );
+        } else {
+            const { last_hit, last_pose } = req.body;
+            const statUpdate = new Stats({
+                stat_id: STATS_ID,
+                last_hit: { ...last_hit },
+                last_pose: { ...last_pose }
+            });
+
+            statUpdate.save((err, r) => {
+                if (!err) {
+                    res.json({ stats: "created", r });
+                }
+            });
+        }
+    });
+});
+
+router.post("/hit", (req, res, next) => {
+    Stats.findOne({ stat_id: STATS_ID }, (err, item) => {
+        if (!!item) {
+            console.log(item);
+            const { last_hit } = req.body;
+
+            Stats.findOneAndUpdate(
+                { stat_id: STATS_ID },
+                { last_hit: { ...last_hit } },
+                (err, r) => {
+                    if (!err) {
+                        res.json({
+                            stats: "updated"
+                        });
+                    } else {
+                        res.json({
+                            error: "error"
+                        });
+                    }
+                }
+            );
+        } else {
+            const { last_hit, last_pose } = req.body;
+            const statUpdate = new Stats({
+                stat_id: STATS_ID,
+                last_hit: { ...last_hit },
+                last_pose: { ...last_pose }
+            });
+
+            statUpdate.save((err, r) => {
+                if (!err) {
+                    res.json({ stats: "created", r });
+                }
+            });
+        }
+    });
+});
+
+router.delete("/", (req, res) => {
+    Stats.findOneAndRemove({ stat_id: STATS_ID }, (err, r) => {
+        if (!err) {
+            res.json({
+                stats: "deleted"
+            });
+        }
+    });
+});
+
+router.post("/", (req, res, next) => {
+    const { last_hit, last_pose } = req.body;
+    const statUpdate = new Stats({
+        last_hit: { ...last_hit },
+        last_pose: { ...last_pose }
+    });
+
+    statUpdate.save((err, r) => {
+        if (!err) {
+            res.json({ stats: "ok" });
+        }
+    });
+});
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  // res.render('index', { title: 'Express' });
-  res.json(sampleRes)
+router.get("/", function(req, res, next) {
+    Stats.findOne({ stat_id: STATS_ID }, (err, data) => {
+        if (!err) {
+            res.json(data);
+            console.log(data);
+        } else {
+            res.send("Error occured");
+        }
+    });
 });
 
 module.exports = router;
