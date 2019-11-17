383ad3a3134b61c3e92b18790d9498858c872e11
Malakhov Dmitriy <mitia2022@gmail.com>
2019-11-17 05:36:49 +0300

    kek

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var cors = require("cors");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+const bodyParser = require('body-parser');
 var app = express();
 
 app.use(cors());
@@ -16,9 +16,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,29 @@ var router = express.Router();
 var mongoose = require("mongoose");
 
 const statsSchema = new mongoose.Schema({
-    stat_id: String,
-    last_hit: {
-        time: Number,
-        x: Number,
-        y: Number,
-        strength: Number,
-        id: Number
-    },
-    last_pose: {
-        time_start: Number,
-        time_end: Number,
-        pose_id: Number
-    }
+  stat_id: String,
+  last_hit: {
+    time: Number,
+    x: Number,
+    y: Number,
+    strength: Number,
+    id: Number
+  },
+  last_pose: {
+    time_start: Number,
+    time_end: Number,
+    pose_id: Number
+  }
 });
 var Stats = mongoose.model("Stats", statsSchema);
 
-var mongoose = require("mongoose"); //process.env.MONGODB_URI
-mongoose.connect(process.env.MONGODB_URI, {
+var mongoose = require("mongoose"); // mongodb://localhost/test
+mongoose.connect(
+  "mongodb://localhost/test", //process.env.MONGODB_URI
+  {
     useNewUrlParser: true
-});
+  }
+);
 
 const STATS_ID = "arngry";
 
@@ -43,186 +46,193 @@ const STATS_ID = "arngry";
 
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function() {
-    // we're connected!
-    console.log("db ready");
+db.once("open", function () {
+  // we're connected!
+  console.log("db ready");
 });
 
 router.post("/pose", (req, res, next) => {
-    Stats.findOne(
-        {
+  Stats.findOne({
+      stat_id: STATS_ID
+    },
+    (err, item) => {
+      if (!!item) {
+        console.log(req.body);
+
+        // const { last_pose } = JSON.parse(req.body);
+
+        const {
+          time_start,
+          time_end,
+          pose_id
+        } = last_pose;
+
+        Stats.findOneAndUpdate({
             stat_id: STATS_ID
-        },
-        (err, item) => {
-            if (!!item) {
-                const { last_pose } = JSON.parse(req.body);
-
-                const { time_start, time_end, pose_id } = last_pose;
-
-                Stats.findOneAndUpdate(
-                    {
-                        stat_id: STATS_ID
-                    },
-                    {
-                        last_pose: {
-                            time_start,
-                            time_end,
-                            pose_id
-                        }
-                    },
-                    (err, r) => {
-                        if (!err) {
-                            console.log(r, last_pose);
-                            res.json({
-                                stats: "updated"
-                            });
-                        } else {
-                            console.log(err);
-                            res.json({
-                                error: "error"
-                            });
-                        }
-                    }
-                );
-            } else {
-                const { last_hit, last_pose } = req.body;
-                const statUpdate = new Stats({
-                    stat_id: STATS_ID,
-                    last_hit: {
-                        ...last_hit
-                    },
-                    last_pose: {
-                        time_start: last_pose.time_start,
-                        time_end: last_pose.time_end,
-                        pose_id: last_pose.pose_id
-                    }
-                });
-
-                statUpdate.save((err, r) => {
-                    if (!err) {
-                        res.json({
-                            stats: "created",
-                            r
-                        });
-                    }
-                });
+          }, {
+            last_pose: {
+              time_start,
+              time_end,
+              pose_id
             }
-        }
-    );
+          },
+          (err, r) => {
+            if (!err) {
+              console.log(r, last_pose);
+              res.json({
+                stats: "updated"
+              });
+            } else {
+              console.log(err);
+              res.json({
+                error: "error"
+              });
+            }
+          }
+        );
+      } else {
+        const {
+          last_hit,
+          last_pose
+        } = req.body;
+        const statUpdate = new Stats({
+          stat_id: STATS_ID,
+          last_hit: {
+            ...last_hit
+          },
+          last_pose: {
+            time_start: last_pose.time_start,
+            time_end: last_pose.time_end,
+            pose_id: last_pose.pose_id
+          }
+        });
+
+        statUpdate.save((err, r) => {
+          if (!err) {
+            res.json({
+              stats: "created",
+              r
+            });
+          }
+        });
+      }
+    }
+  );
 });
 
 router.get("/hit/:x/:y/:time", (req, res, next) => {
-    Stats.findOne(
-        {
+  Stats.findOne({
+      stat_id: STATS_ID
+    },
+    (err, item) => {
+      if (!!item) {
+        Stats.findOneAndUpdate({
             stat_id: STATS_ID
-        },
-        (err, item) => {
-            if (!!item) {
-                Stats.findOneAndUpdate(
-                    {
-                        stat_id: STATS_ID
-                    },
-                    {
-                        last_hit: {
-                            x: req.params.x,
-                            y: req.params.y,
-                            time: req.params.time
-                        }
-                    },
-                    (err, r) => {
-                        if (!err) {
-                            res.json({
-                                stats: "updated"
-                            });
-                        } else {
-                            res.json({
-                                error: "error"
-                            });
-                        }
-                    }
-                );
-            } else {
-                const statUpdate = new Stats({
-                    stat_id: STATS_ID,
-                    last_hit: {
-                        x: req.params.x,
-                        y: req.params.y,
-                        time: req.params.time
-                    }
-                });
-
-                statUpdate.save((err, r) => {
-                    if (!err) {
-                        res.json({
-                            stats: "created",
-                            r
-                        });
-                    }
-                });
+          }, {
+            last_hit: {
+              x: req.params.x,
+              y: req.params.y,
+              time: req.params.time
             }
-        }
-    );
+          },
+          (err, r) => {
+            if (!err) {
+              res.json({
+                stats: "updated"
+              });
+            } else {
+              res.json({
+                error: "error"
+              });
+            }
+          }
+        );
+      } else {
+        const statUpdate = new Stats({
+          stat_id: STATS_ID,
+          last_hit: {
+            x: req.params.x,
+            y: req.params.y,
+            time: req.params.time
+          }
+        });
+
+        statUpdate.save((err, r) => {
+          if (!err) {
+            res.json({
+              stats: "created",
+              r
+            });
+          }
+        });
+      }
+    }
+  );
 });
 
 router.delete("/", (req, res) => {
-    Stats.findOneAndRemove(
-        {
-            stat_id: STATS_ID
-        },
-        (err, r) => {
-            if (!err) {
-                res.json({
-                    stats: "deleted"
-                });
-            }
-        }
-    );
+  Stats.findOneAndRemove({
+      stat_id: STATS_ID
+    },
+    (err, r) => {
+      if (!err) {
+        res.json({
+          stats: "deleted"
+        });
+      }
+    }
+  );
 });
 
 router.post("/", (req, res, next) => {
-    const { last_hit, last_pose } = req.body;
-    const statUpdate = new Stats({
-        last_hit: {
-            ...last_hit
-        },
-        last_pose: {
-            ...last_pose
-        }
-    });
+  const {
+    last_hit,
+    last_pose
+  } = req.body;
+  const statUpdate = new Stats({
+    last_hit: {
+      ...last_hit
+    },
+    last_pose: {
+      ...last_pose
+    }
+  });
 
-    statUpdate.save((err, r) => {
-        if (!err) {
-            res.json({
-                stats: "ok"
-            });
-        }
-    });
+  statUpdate.save((err, r) => {
+    if (!err) {
+      res.json({
+        stats: "ok"
+      });
+    }
+  });
 });
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-    Stats.findOne(
-        {
-            stat_id: STATS_ID
-        },
-        (err, data) => {
-            if (!err) {
-                res.json(data);
-                console.log(data);
-            } else {
-                res.send("Error occured");
-            }
-        }
-    );
+router.get("/", function (req, res, next) {
+  Stats.findOne({
+      stat_id: STATS_ID
+    },
+    (err, data) => {
+      if (!err) {
+        res.json(data);
+        console.log(data);
+      } else {
+        res.send("Error occured");
+      }
+    }
+  );
 });
 
 /* GET home page. */
-router.get("/:bird/:flag", function(req, res, next) {
-    const { bird, flag } = req.params;
+router.get("/:bird/:flag", function (req, res, next) {
+  const {
+    bird,
+    flag
+  } = req.params;
 
-    res.json({
-        message: `flag ${flag} activated for bird ${bird}`
-    });
+  res.json({
+    message: `flag ${flag} activated for bird ${bird}`
+  });
 });
 
 module.exports = router;
